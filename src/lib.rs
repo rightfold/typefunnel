27bab6e684c7b7f2164f871b8079a4a307bbe8f3
rightfold <rightfold@gmail.com>
2017-04-23 14:37:55 +0200

    Add Schema type with products, sums, and some primitives

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,30 @@
 //! assets from these schemas. Examples of sources are SQL queries, WSDL files,
 //! and static data. Examples of assets are user interfaces, web services,
 //! serialization, and documentation.
+
+/// A schema describes the structure of data. Schemas are generated from
+/// sources, and are used to generate web services, serialization functions,
+/// user interfaces, and so on.
+pub enum Schema {
+  /// The data described is a heterogeneous sequence of values.
+  AllOf(Vec<Schema>),
+
+  /// The data described conforms to one of many schemas.
+  OneOf(Vec<Schema>),
+
+  /// The data described is an integer with a lower and upper bound (both
+  /// inclusive).
+  SignedInteger(i32, i32),
+
+  /// The data described is a single-precision floating-point number.
+  SinglePrecision,
+
+  /// The data described is a double-precision floating-point number.
+  DoublePrecision,
+
+  /// The data described is of arbitrary format.
+  ByteString,
+
+  /// The data described is arbitrary Unicode text.
+  String,
+}
