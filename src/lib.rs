a62ec3ec128b02eb7c1b2f358332b50bfacc3c49
rightfold <rightfold@gmail.com>
2017-04-23 14:37:41 +0200

    Add typefunnel crate

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+#![warn(missing_docs)]
+
+//! TypeFunnel is a tool for extracting schemas from sources and generating
+//! assets from these schemas. Examples of sources are SQL queries, WSDL files,
+//! and static data. Examples of assets are user interfaces, web services,
+//! serialization, and documentation.
